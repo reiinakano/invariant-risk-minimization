2a00a2f6e7448f751d690323cad225318f5b2d28
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-08-26 01:59:41 +0900

    add plotting

--- main.py
+++ main.py
@@ -320,6 +320,23 @@ def train_and_test_erm():
     test(model, device, test_loader)
 
 
+def plot_dataset_digits(dataset):
+  fig = plt.figure(figsize=(13, 16))
+  columns = 6
+  rows = 6
+  # ax enables access to manipulate each of subplots
+  ax = []
+
+  for i in range(columns * rows):
+    img, label = dataset[i]
+    # create subplot and append to ax
+    ax.append(fig.add_subplot(rows, columns, i + 1))
+    ax[-1].set_title("Label: " + str(label))  # set title
+    plt.imshow(img)
+
+  plt.show()  # finally, render the plot
+
+
 def main():
   train_and_test_irm()
 
