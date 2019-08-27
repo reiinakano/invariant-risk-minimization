db0111b5fc5cd0d499d30d29bdba99d7f1b45978
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-08-28 00:15:46 +0900

    refactor

---
+++ colored_mnist.py
@@ -0,0 +1,136 @@
+import os
+
+import numpy as np
+from PIL import Image
+
+import torch
+from torchvision import datasets
+import torchvision.datasets.utils as dataset_utils
+
+
+def color_grayscale_arr(arr, red=True):
+  """Converts grayscale image to either red or green"""
+  assert arr.ndim == 2
+  dtype = arr.dtype
+  h, w = arr.shape
+  arr = np.reshape(arr, [h, w, 1])
+  if red:
+    arr = np.concatenate([arr,
+                          np.zeros((h, w, 2), dtype=dtype)], axis=2)
+  else:
+    arr = np.concatenate([np.zeros((h, w, 1), dtype=dtype),
+                          arr,
+                          np.zeros((h, w, 1), dtype=dtype)], axis=2)
+  return arr
+
+
+class ColoredMNIST(datasets.VisionDataset):
+  """
+  Colored MNIST dataset for testing IRM. Prepared using procedure from https://arxiv.org/pdf/1907.02893.pdf
+
+  Args:
+    root (string): Root directory of dataset where ``ColoredMNIST/*.pt`` will exist.
+    env (string): Which environment to load. Must be 1 of 'train1', 'train2', 'test', or 'all_train'.
+    transform (callable, optional): A function/transform that  takes in an PIL image
+      and returns a transformed version. E.g, ``transforms.RandomCrop``
+    target_transform (callable, optional): A function/transform that takes in the
+      target and transforms it.
+  """
+  def __init__(self, root='./data', env='train1', transform=None, target_transform=None):
+    super(ColoredMNIST, self).__init__(root, transform=transform,
+                                target_transform=target_transform)
+
+    self.prepare_colored_mnist()
+    if env in ['train1', 'train2', 'test']:
+      self.data_label_tuples = torch.load(os.path.join(self.root, 'ColoredMNIST', env) + '.pt')
+    elif env == 'all_train':
+      self.data_label_tuples = torch.load(os.path.join(self.root, 'ColoredMNIST', 'train1.pt')) + \
+                               torch.load(os.path.join(self.root, 'ColoredMNIST', 'train2.pt'))
+    else:
+      raise RuntimeError(f'{env} env unknown. Valid envs are train1, train2, test, and all_train')
+
+  def __getitem__(self, index):
+    """
+    Args:
+        index (int): Index
+
+    Returns:
+        tuple: (image, target) where target is index of the target class.
+    """
+    img, target = self.data_label_tuples[index]
+
+    if self.transform is not None:
+      img = self.transform(img)
+
+    if self.target_transform is not None:
+      target = self.target_transform(target)
+
+    return img, target
+
+  def __len__(self):
+    return len(self.data_label_tuples)
+
+  def prepare_colored_mnist(self):
+    colored_mnist_dir = os.path.join(self.root, 'ColoredMNIST')
+    if os.path.exists(os.path.join(colored_mnist_dir, 'train1.pt')) \
+        and os.path.exists(os.path.join(colored_mnist_dir, 'train2.pt')) \
+        and os.path.exists(os.path.join(colored_mnist_dir, 'test.pt')):
+      print('Colored MNIST dataset already exists')
+      return
+
+    print('Preparing Colored MNIST')
+    train_mnist = datasets.mnist.MNIST(self.root, train=True, download=True)
+
+    train1_set = []
+    train2_set = []
+    test_set = []
+    for idx, (im, label) in enumerate(train_mnist):
+      if idx % 10000 == 0:
+        print(f'Converting image {idx}/{len(train_mnist)}')
+      im_array = np.array(im)
+
+      # Assign a binary label y to the image based on the digit
+      binary_label = 0 if label < 5 else 1
+
+      # Flip label with 25% probability
+      if np.random.uniform() < 0.25:
+        binary_label = binary_label ^ 1
+
+      # Color the image either red or green according to its possibly flipped label
+      color_red = binary_label == 0
+
+      # Flip the color with a probability e that depends on the environment
+      if idx < 20000:
+        # 20% in the first training environment
+        if np.random.uniform() < 0.2:
+          color_red = not color_red
+      elif idx < 40000:
+        # 10% in the first training environment
+        if np.random.uniform() < 0.1:
+          color_red = not color_red
+      else:
+        # 90% in the test environment
+        if np.random.uniform() < 0.9:
+          color_red = not color_red
+
+      colored_arr = color_grayscale_arr(im_array, red=color_red)
+
+      if idx < 20000:
+        train1_set.append((Image.fromarray(colored_arr), binary_label))
+      elif idx < 40000:
+        train2_set.append((Image.fromarray(colored_arr), binary_label))
+      else:
+        test_set.append((Image.fromarray(colored_arr), binary_label))
+
+      # Debug
+      # print('original label', type(label), label)
+      # print('binary label', binary_label)
+      # print('assigned color', 'red' if color_red else 'green')
+      # plt.imshow(colored_arr)
+      # plt.show()
+      # break
+
+    dataset_utils.makedir_exist_ok(colored_mnist_dir)
+    torch.save(train1_set, os.path.join(colored_mnist_dir, 'train1.pt'))
+    torch.save(train2_set, os.path.join(colored_mnist_dir, 'train2.pt'))
+    torch.save(test_set, os.path.join(colored_mnist_dir, 'test.pt'))
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
-
-import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
 
 import torch
@@ -9,136 +5,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import grad
-from torchvision import datasets, transforms
-import torchvision.datasets.utils as dataset_utils
+from torchvision import transforms
 
-
-def color_grayscale_arr(arr, red=True):
-  """Converts grayscale image to either red or green"""
-  assert arr.ndim == 2
-  dtype = arr.dtype
-  h, w = arr.shape
-  arr = np.reshape(arr, [h, w, 1])
-  if red:
-    arr = np.concatenate([arr,
-                          np.zeros((h, w, 2), dtype=dtype)], axis=2)
-  else:
-    arr = np.concatenate([np.zeros((h, w, 1), dtype=dtype),
-                          arr,
-                          np.zeros((h, w, 1), dtype=dtype)], axis=2)
-  return arr
-
-
-class ColoredMNIST(datasets.VisionDataset):
-  """
-  Colored MNIST dataset for testing IRM. Prepared using procedure from https://arxiv.org/pdf/1907.02893.pdf
-
-  Args:
-    root (string): Root directory of dataset where ``ColoredMNIST/*.pt`` will exist.
-    env (string): Which environment to load. Must be 1 of 'train1', 'train2', 'test', or 'all_train'.
-    transform (callable, optional): A function/transform that  takes in an PIL image
-      and returns a transformed version. E.g, ``transforms.RandomCrop``
-    target_transform (callable, optional): A function/transform that takes in the
-      target and transforms it.
-  """
-  def __init__(self, root='./data', env='train1', transform=None, target_transform=None):
-    super(ColoredMNIST, self).__init__(root, transform=transform,
-                                target_transform=target_transform)
-
-    self.prepare_colored_mnist()
-    if env in ['train1', 'train2', 'test']:
-      self.data_label_tuples = torch.load(os.path.join(self.root, 'ColoredMNIST', env) + '.pt')
-    elif env == 'all_train':
-      self.data_label_tuples = torch.load(os.path.join(self.root, 'ColoredMNIST', 'train1.pt')) + \
-                               torch.load(os.path.join(self.root, 'ColoredMNIST', 'train2.pt'))
-    else:
-      raise RuntimeError(f'{env} env unknown. Valid envs are train1, train2, test, and all_train')
-
-  def __getitem__(self, index):
-    """
-    Args:
-        index (int): Index
-
-    Returns:
-        tuple: (image, target) where target is index of the target class.
-    """
-    img, target = self.data_label_tuples[index]
-
-    if self.transform is not None:
-      img = self.transform(img)
-
-    if self.target_transform is not None:
-      target = self.target_transform(target)
-
-    return img, target
-
-  def __len__(self):
-    return len(self.data_label_tuples)
-
-  def prepare_colored_mnist(self):
-    colored_mnist_dir = os.path.join(self.root, 'ColoredMNIST')
-    if os.path.exists(os.path.join(colored_mnist_dir, 'train1.pt')) \
-        and os.path.exists(os.path.join(colored_mnist_dir, 'train2.pt')) \
-        and os.path.exists(os.path.join(colored_mnist_dir, 'test.pt')):
-      print('Colored MNIST dataset already exists')
-      return
-
-    print('Preparing Colored MNIST')
-    train_mnist = datasets.mnist.MNIST(self.root, train=True, download=True)
-
-    train1_set = []
-    train2_set = []
-    test_set = []
-    for idx, (im, label) in enumerate(train_mnist):
-      if idx % 10000 == 0:
-        print(f'Converting image {idx}/{len(train_mnist)}')
-      im_array = np.array(im)
-
-      # Assign a binary label y to the image based on the digit
-      binary_label = 0 if label < 5 else 1
-
-      # Flip label with 25% probability
-      if np.random.uniform() < 0.25:
-        binary_label = binary_label ^ 1
-
-      # Color the image either red or green according to its possibly flipped label
-      color_red = binary_label == 0
-
-      # Flip the color with a probability e that depends on the environment
-      if idx < 20000:
-        # 20% in the first training environment
-        if np.random.uniform() < 0.2:
-          color_red = not color_red
-      elif idx < 40000:
-        # 10% in the first training environment
-        if np.random.uniform() < 0.1:
-          color_red = not color_red
-      else:
-        # 90% in the test environment
-        if np.random.uniform() < 0.9:
-          color_red = not color_red
-
-      colored_arr = color_grayscale_arr(im_array, red=color_red)
-
-      if idx < 20000:
-        train1_set.append((Image.fromarray(colored_arr), binary_label))
-      elif idx < 40000:
-        train2_set.append((Image.fromarray(colored_arr), binary_label))
-      else:
-        test_set.append((Image.fromarray(colored_arr), binary_label))
-
-      # Debug
-      # print('original label', type(label), label)
-      # print('binary label', binary_label)
-      # print('assigned color', 'red' if color_red else 'green')
-      # plt.imshow(colored_arr)
-      # plt.show()
-      # break
-
-    dataset_utils.makedir_exist_ok(colored_mnist_dir)
-    torch.save(train1_set, os.path.join(colored_mnist_dir, 'train1.pt'))
-    torch.save(train2_set, os.path.join(colored_mnist_dir, 'train2.pt'))
-    torch.save(test_set, os.path.join(colored_mnist_dir, 'test.pt'))
+from colored_mnist import ColoredMNIST
 
 
 class Net(nn.Module):
@@ -175,22 +44,7 @@ class ConvNet(nn.Module):
     return logits
 
 
-def erm_train(model, device, train_loader, optimizer, epoch):
-  model.train()
-  for batch_idx, (data, target) in enumerate(train_loader):
-    data, target = data.to(device), target.to(device).float()
-    optimizer.zero_grad()
-    output = model(data)
-    loss = F.binary_cross_entropy_with_logits(output, target)
-    loss.backward()
-    optimizer.step()
-    if batch_idx % 10 == 0:
-      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        epoch, batch_idx * len(data), len(train_loader.dataset),
-               100. * batch_idx / len(train_loader), loss.item()))
-
-
-def test(model, device, test_loader):
+def test_model(model, device, test_loader):
   model.eval()
   test_loss = 0
   correct = 0
@@ -211,7 +65,53 @@ def test(model, device, test_loader):
     100. * correct / len(test_loader.dataset)))
 
 
-def compute_penalty(losses, dummy):
+def erm_train(model, device, train_loader, optimizer, epoch):
+  model.train()
+  for batch_idx, (data, target) in enumerate(train_loader):
+    data, target = data.to(device), target.to(device).float()
+    optimizer.zero_grad()
+    output = model(data)
+    loss = F.binary_cross_entropy_with_logits(output, target)
+    loss.backward()
+    optimizer.step()
+    if batch_idx % 10 == 0:
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        epoch, batch_idx * len(data), len(train_loader.dataset),
+               100. * batch_idx / len(train_loader), loss.item()))
+
+
+def train_and_test_erm():
+  use_cuda = torch.cuda.is_available()
+  device = torch.device("cuda" if use_cuda else "cpu")
+
+  kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+  all_train_loader = torch.utils.data.DataLoader(
+    ColoredMNIST(root='./data', env='all_train',
+                 transform=transforms.Compose([
+                     transforms.ToTensor(),
+                     transforms.Normalize((0.1307, 0.1307, 0.), (0.3081, 0.3081, 0.3081))
+                   ])),
+    batch_size=64, shuffle=True, **kwargs)
+
+  test_loader = torch.utils.data.DataLoader(
+    ColoredMNIST(root='./data', env='test', transform=transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.1307, 0.1307, 0.), (0.3081, 0.3081, 0.3081))
+    ])),
+    batch_size=1000, shuffle=True, **kwargs)
+
+  model = ConvNet().to(device)
+  optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+  for epoch in range(1, 10):
+    erm_train(model, device, all_train_loader, optimizer, epoch)
+    print('testing on train set')
+    test_model(model, device, all_train_loader)
+    print('testing on test set')
+    test_model(model, device, test_loader)
+
+
+def compute_irm_penalty(losses, dummy):
   g1 = grad(losses[0::2].mean(), dummy, create_graph=True)[0]
   g2 = grad(losses[1::2].mean(), dummy, create_graph=True)[0]
   return (g1 * g2).sum()
@@ -238,7 +138,7 @@ def irm_train(model, device, train_loaders, optimizer, epoch):
       data, target = data.to(device), target.to(device).float()
       output = model(data)
       loss_erm = F.binary_cross_entropy_with_logits(output * dummy_w, target, reduction='none')
-      penalty += compute_penalty(loss_erm, dummy_w)
+      penalty += compute_irm_penalty(loss_erm, dummy_w)
       error += loss_erm.mean()
     (error + penalty_multiplier * penalty).backward()
     optimizer.step()
@@ -285,42 +185,11 @@ def train_and_test_irm():
   for epoch in range(1, 100):
     irm_train(model, device, [train1_loader, train2_loader], optimizer, epoch)
     print('testing on train1 set')
-    test(model, device, train1_loader)
+    test_model(model, device, train1_loader)
     print('testing on train2 set')
-    test(model, device, train2_loader)
+    test_model(model, device, train2_loader)
     print('testing on test set')
-    test(model, device, test_loader)
-
-
-def train_and_test_erm():
-  use_cuda = torch.cuda.is_available()
-  device = torch.device("cuda" if use_cuda else "cpu")
-
-  kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-  all_train_loader = torch.utils.data.DataLoader(
-    ColoredMNIST(root='./data', env='all_train',
-                 transform=transforms.Compose([
-                     transforms.ToTensor(),
-                     transforms.Normalize((0.1307, 0.1307, 0.), (0.3081, 0.3081, 0.3081))
-                   ])),
-    batch_size=64, shuffle=True, **kwargs)
-
-  test_loader = torch.utils.data.DataLoader(
-    ColoredMNIST(root='./data', env='test', transform=transforms.Compose([
-      transforms.ToTensor(),
-      transforms.Normalize((0.1307, 0.1307, 0.), (0.3081, 0.3081, 0.3081))
-    ])),
-    batch_size=1000, shuffle=True, **kwargs)
-
-  model = ConvNet().to(device)
-  optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-  for epoch in range(1, 10):
-    erm_train(model, device, all_train_loader, optimizer, epoch)
-    print('testing on train set')
-    test(model, device, all_train_loader)
-    print('testing on test set')
-    test(model, device, test_loader)
+    test_model(model, device, test_loader)
 
 
 def plot_dataset_digits(dataset):
@@ -342,6 +211,7 @@ def plot_dataset_digits(dataset):
 
 def main():
   train_and_test_irm()
+  # train_and_test_erm()
 
 
 if __name__ == '__main__':
